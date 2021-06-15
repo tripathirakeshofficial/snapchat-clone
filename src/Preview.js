a51b9717e7abe6adc04f390f4251c1200c839702
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-06-15 22:24:26 +0530

    ADD: Preview Page

--- src/Preview.js
+++ src/Preview.js
@@ -1,8 +1,89 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
 import "./Preview.css";
+import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import NoteIcon from "@material-ui/icons/Note";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import SendIcon from "@material-ui/icons/Send";
+import { v4 as uuid } from "uuid";
+import { db, storage } from "./firebase";
+import firebase from "firebase";
 
 function Preview() {
-  return <div className="preview">Preview</div>;
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+    history.replace("/");
+  };
+
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, "data_url");
+
+    uploadTask.on(
+      "state_chenged",
+      null,
+      (error) => {
+        //ERROR function
+        console.log(error);
+      },
+      () => {
+        //COMPLETE function
+        storage
+          .ref("posts")
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              imageUrl: url,
+              username: "Funny Santa",
+              read: false,
+              //profilePic
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            history.replace("/chats");
+          });
+      }
+    );
+  };
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview_close" />
+      <div className="preview_toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
+      <img src={cameraImage} alt="" />
+      <div onClick={sendPost} className="preview_footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize="small" className="preview_sendIcon" />
+      </div>
+    </div>
+  );
 }
 
 export default Preview;
