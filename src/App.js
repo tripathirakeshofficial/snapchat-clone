81a92559663ff20cbb4ce8ea5d0a37a034878231
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-06-19 13:52:05 +0530

    ADD: Login Page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import { login, logout, selectUser } from "./features/appSlice";
+import { auth } from "./firebase";
+import Login from "./Login";
 import Preview from "./Preview";
 import WebcamCapture from "./WebcamCapture";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoUrl,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
-        <div className="app_body">
-          <Switch>
-            <Route path="/preview">
-              <Preview />
-            </Route>
-            <Route path="/chats/view">
-              <ChatView />
-            </Route>
-            <Route path="/chats">
-              <Chats />
-            </Route>
-            <Route exact path="/">
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app_body">
+            <Switch>
+              <Route path="/preview">
+                <Preview />
+              </Route>
+              <Route path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route path="/chats">
+                <Chats />
+              </Route>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/appSlice";
+import { auth, provider } from "./firebase";
+import "./Login.css";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch(
+          login({
+            username: result.user.displayName,
+            profilePic: result.user.photoUrl,
+            id: result.user.uid,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt="" />
+        <Button variant="outline" onClick={signIn}>
+          Sign in
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
